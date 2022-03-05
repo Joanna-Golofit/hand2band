96c3a6e05718a769d167b51c8338add6817752a9
Joanna-Golofit <sunderlajsa@gmail.com>
2022-03-05 18:04:05 +0100

    miało być jako modalne... a nie osobna strona

--- src/pages/ImageDetailsPage/ImageDetailsPage.jsx
+++ src/pages/ImageDetailsPage/ImageDetailsPage.jsx
@@ -2,15 +2,21 @@ import React from "react";
 import { useState } from "react";
 import { useEffect } from "react";
 import fetchImages from "../../utils/fetchImages";
-import { Link, Outlet, useLocation, useNavigate, useParams } from "react-router-dom";
+import {
+  Link,
+  Outlet,
+  useLocation,
+  useNavigate,
+  useParams,
+} from "react-router-dom";
+import styles from "./ImageDetailsPage.module.css";
 
 const ImageDetailsPage = () => {
   const location = useLocation();
   const [imgData, setImgData] = useState(null);
-// const { imageId } = useParams();
+  // const { imageId } = useParams();
   // const { image, setImage } = useState([]);
   // const URL = `https://api.unsplash.com/search/photos/${imageId}&client_id=gK52De2Tm_dL5o1IXKa9FROBAJ-LIYqR41xBdlg3X2k`;
-
 
   useEffect(() => {
     setImgData(location.state.data);
@@ -33,17 +39,21 @@ const ImageDetailsPage = () => {
     <div>
       ImageDetailsPage
       {imgData && (
-        <div>
-          <p>{imgData.id}</p>
-          <p>{imgData.created_at}</p>
-          <p>{imgData.updated_at}</p>
-          <p>{imgData.user.name}</p>
-          <p>{imgData.user.location}</p>
-          <img
-            src={imgData.urls.regular}
-            alt={imgData.alt_description}
-            className="wow"
-          />
+        <div className={styles.overlay}>
+          <div className={styles.modal}>
+            <p>{imgData.id}</p>
+            <p>{imgData.created_at}</p>
+            <p>{imgData.created_at.slice(0, 4)}</p>
+            <p>{imgData.created_at.slice(5, 7)}</p>
+            <p>{imgData.updated_at}</p>
+            <p>{imgData.user.name}</p>
+            <p>{imgData.user.location}</p>
+            <img
+              src={imgData.urls.regular}
+              alt={imgData.alt_description}
+              className={styles.img}
+            />
+          </div>
         </div>
       )}
     </div>
