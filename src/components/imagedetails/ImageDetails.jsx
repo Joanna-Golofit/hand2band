edd629366eedb8ea9d65d577de52b74148ba8431
Joanna-Golofit <sunderlajsa@gmail.com>
2022-03-08 14:33:33 +0100

    switch statement

--- src/components/imagedetails/ImageDetails.jsx
+++ src/components/imagedetails/ImageDetails.jsx
@@ -6,24 +6,60 @@ import styles from "./ImageDetails.module.css";
 
 const ImageDetails = ({ imgData }) => {
   const { modal, setModal } = useContext(ModalContext);
-  
- const handleClick = () => {
-   setModal(!modal);
- };
+
+  const renderSwitch = (param) => {
+    switch (param) {
+      case "01":
+        return "January";
+      case "02":
+        return "February";
+      case "03":
+        return "March";
+      case "04":
+        return "April";
+      case "05":
+        return "May";
+      case "06":
+        return "June";
+      case "07":
+        return "July";
+      case "08":
+        return "August";
+      case "09":
+        return "September";
+      case "10":
+        return "October";
+      case "11":
+        return "November";
+      case "12":
+        return "December";
+      default:
+        return "oops";
+    }
+  }
+
+  const handleClick = () => {
+    setModal(!modal);
+  };
 
   return (
     <div>
       {imgData && (
         <div className={styles.overlay} onClick={handleClick}>
           <div className={styles.modal}>
-            <p>{imgData.id}</p>
-            <p>{imgData.created_at}</p>
-            <p>{imgData.created_at.slice(0, 4)}</p>
-            {/* <p>{renderSwitch(imgData.created_at.slice(5, 7))}</p> */}
-            <p>{imgData.created_at.slice(5, 7)}</p>
-            <p>{imgData.updated_at}</p>
-            <p>{imgData.user.name}</p>
-            <p>{imgData.user.location}</p>
+            <h4>Author: {imgData.user.name}</h4>
+            {/* <p>{imgData.id}</p> */}
+            {/* <p>Created at: {imgData.created_at}</p> */}
+            <p>
+              Created at: {renderSwitch(imgData.created_at.slice(5, 7))}{" "}
+              {imgData.created_at.slice(0, 4)}
+            </p>
+            {/* <p>Added at: {imgData.updated_at}</p> */}
+            <p>
+              Added at: {renderSwitch(imgData.updated_at.slice(5, 7))}{" "}
+              {imgData.updated_at.slice(0, 4)}
+            </p>
+            <p>Location: {imgData.user.location}</p>
             <img
               src={imgData.urls.regular}
               alt={imgData.alt_description}
